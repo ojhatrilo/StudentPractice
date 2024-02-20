e6d3ec9a39cee3d88d51ded41e9a0c6e9ceb74fc
ojhatrilo <115971421+ojhatrilo@users.noreply.github.com>
2024-02-20 12:41:09 +0530

    def function

---
+++ Python Daily Studies/Functions.py
@@ -0,0 +1,72 @@
+# To Create Fuction we use "def" keyword followed by the name of the function
+
+# def hello():
+#     print("iam hello function")
+#
+# def hi():
+#     print("belongs to Hi function")
+#
+# hello()
+# hi()
+
+# def sai():
+#     return "iam sai function"
+#
+#
+# print(sai())
+
+# def add(num1,num2,num3):
+#     return num1+num2
+#
+#
+#
+# a = int(input("Enter the number:"))
+# b = int(input("Enter the number:"))
+#
+# print(add(a,b,3))
+
+# Function Scope
+
+# a = 24
+#
+# def numacess():
+#    global a
+#    a+=3
+#    print(a)
+#
+# numacess()
+
+# Default Argument
+
+# def cyber(num1=2,num2=3):
+#    return num1+num2
+#
+# print(cyber(3,5))
+#
+
+# def multiple_items(*args):
+#    print(args)
+#    print(type(args))
+#
+#
+# multiple_items("Dave", "John", "Sara")
+
+
+# i = 0
+# secrets = 5
+# while (i<3):
+#    num1 = int(input("Enter the number:"))
+#    if num1==secrets:
+#       print("you win")
+#       break
+#    else:
+#       print("your guess is wrong")
+#       i+=1
+
+
+
+
+
+
+
+
