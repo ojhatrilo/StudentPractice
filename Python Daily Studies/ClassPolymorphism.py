c84d475f915dfe80ea237b75950aff552296b40c
Ojhatrilo <ojhatrilochan5@gmail.com>
2024-03-14 19:27:10 +0530

    update

--- Python Daily Studies/ClassPolymorphism.py
+++ Python Daily Studies/ClassPolymorphism.py
@@ -11,9 +11,9 @@
 
 # # A simple Python function to demonstrate
 # # Polymorphism
-#
+
 # def add(x, y, z = 0):
-# 	return x + y+z
+# 	return x + y + z
 #
 # # Driver code
 # print(add(2, 3))
@@ -21,10 +21,11 @@
 
 
 
-#
+
 # class India:
 # 	def capital(self):
 # 		print("New Delhi is the capital of India.")
+#
 #
 # 	def language(self):
 # 		print("Hindi is the most widely spoken language of India.")
@@ -48,42 +49,42 @@
 # 	country.capital()
 # 	country.language()
 # 	country.type()
+#
+# #
+# #
 
 
 
+class India():
+	def capital(self):
+		print("New Delhi is the capital of India.")
 
+	def language(self):
+		print("Hindi is the most widely spoken language of India.")
 
-#
-# class India():
-# 	def capital(self):
-# 		print("New Delhi is the capital of India.")
-#
-# 	def language(self):
-# 		print("Hindi is the most widely spoken language of India.")
-#
-# 	def type(self):
-# 		print("India is a developing country.")
-#
-# class USA():
-# 	def capital(self):
-# 		print("Washington, D.C. is the capital of USA.")
-#
-# 	def language(self):
-# 		print("English is the primary language of USA.")
-#
-# 	def type(self):
-# 		print("USA is a developed country.")
-#
-# def func(obj):
-# 	obj.capital()
-# 	obj.language()
-# 	obj.type()
-#
-# obj_ind = India()
-# obj_usa = USA()
-#
-# func(obj_ind)
-# func(obj_usa)
+	def type(self):
+		print("India is a developing country.")
+
+class USA():
+	def capital(self):
+		print("Washington, D.C. is the capital of USA.")
+
+	def language(self):
+		print("English is the primary language of USA.")
+
+	def type(self):
+		print("USA is a developed country.")
+
+def func(obj):
+	obj.capital()
+	obj.language()
+	obj.type()
+
+obj_ind = India()
+obj_usa = USA()
+
+func(obj_ind)
+func(obj_usa)
 
 
 # class Bird:
--- Python Daily Studies/classobj.py
+++ Python Daily Studies/classobj.py
@@ -36,35 +36,35 @@
 
 # Inheritance
 # Single Inheritance
-class add:
-    def __init__(self,num1,num2):
-        self.num1 = num1
-        self.num2 = num2
-
-    def addition(self):
-        return self.num1 + self.num2
-
-class sub(add):
-    # def __init__(self,num1,num2):
-    #     self.num1 = num1
-    #     self.num2 = num2
-
-    def subtraction(self):
-        return self.num1 - self.num2
-
-
-while True:
-    a = int(input("enter the number: "))
-    b = int(input("enter the number2: "))
-    options = input("enter the add-1 and sub-2:")
-    if options == "1":
-        Add1 = sub(a,b)
-        print(Add1.addition())
-    elif options == "2":
-        Sub1 = sub(a,b)
-        print(Sub1.subtraction())
-    else:
-        break
+# class add:
+#     def __init__(self,num1,num2):
+#         self.num1 = num1
+#         self.num2 = num2
+#
+#     def addition(self):
+#         return self.num1 + self.num2
+#
+# class sub(add):
+#     # def __init__(self,num1,num2):
+#     #     self.num1 = num1
+#     #     self.num2 = num2
+#
+#     def subtraction(self):
+#         return self.num1 - self.num2
+#
+#
+# while True:
+#     a = int(input("enter the number: "))
+#     b = int(input("enter the number2: "))
+#     options = input("enter the add-1 and sub-2:")
+#     if options == "1":
+#         Add1 = sub(a,b)
+#         print(Add1.addition())
+#     elif options == "2":
+#         Sub1 = sub(a,b)
+#         print(Sub1.subtraction())
+#     else:
+#         break
 
 
 
@@ -99,7 +99,6 @@ while True:
 # class div(mul):
 #     def division(self):
 #         return self.num2/self.num1
-#
 #
 #
 # while True:
