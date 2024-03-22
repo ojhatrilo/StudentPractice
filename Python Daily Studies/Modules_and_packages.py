8081377a07f0c044aa9d1b8c2d71283ef98a7075
ojhatrilo <ojhatrilochan5@gmail.com>
2024-03-22 12:50:42 +0530

    pakages

---
+++ Python Daily Studies/Modules_and_packages.py
@@ -0,0 +1,25 @@
+# import package as pk
+
+# Add1 = pk.add(2,3)
+# print(Add1.addition())
+#
+# pk.options()
+
+
+# from package import *
+import package as pk
+# options()
+# a = pk.person1["age"]
+
+# pk.person1['name']='ranjani'
+# print(pk.person1)
+
+# import sys
+#
+# sys.path.append("D://StudentPractice//modules_package")
+#
+# import hello
+# hello.hi()
+
+
+
--- Python Daily Studies/function in bank.py
+++ Python Daily Studies/function in bank.py
@@ -1,38 +1,38 @@
-# amount  = 0
-#
-# def withdrawl(user):
-#     global amount
-#     if user > amount:
-#         print("In Sufficient",amount)
-#     else:
-#         print(user)
-#         amount-=user
-#         print("balance",amount)
-#
-# def deposite(user):
-#     global amount
-#     amount+=user
-#     print("balance",amount)
-#
-# def balance():
-#     global amount
-#     print("Available",amount)
-#
-# while(True):
-#     print('''
-#             press - 1 to deposite
-#             press - 2 to withdrawl
-#             press - 3 to balance
-#             press - Q to exit
-#                                       ''')
-#     options = input("Enter the number:").upper()
-#     if options == "1":
-#         num1 = int(input("enter the nuber:"))
-#         deposite(num1)
-#     elif options == "2":
-#         num1 = int(input("enter the nuber:"))
-#         withdrawl(num1)
-#     elif options == "3":
-#         balance()
-#     elif options == "Q":
-#         break
+amount  = 0
+
+def withdrawl(user):
+    global amount
+    if user > amount:
+        print("In Sufficient",amount)
+    else:
+        print(user)
+        amount-=user
+        print("balance",amount)
+
+def deposite(user):
+    global amount
+    amount+=user
+    print("balance",amount)
+
+def balance():
+    global amount
+    print("Available",amount)
+
+while(True):
+    print('''
+            press - 1 to deposite
+            press - 2 to withdrawl
+            press - 3 to balance
+            press - Q to exit
+                                      ''')
+    options = input("Enter the number:").upper()
+    if options == "1":
+        num1 = int(input("enter the nuber:"))
+        deposite(num1)
+    elif options == "2":
+        num1 = int(input("enter the nuber:"))
+        withdrawl(num1)
+    elif options == "3":
+        balance()
+    elif options == "Q":
+        break
