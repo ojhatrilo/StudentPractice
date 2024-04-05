c85b32f9e1c132d496bd8aa81012dedacace9555
Ojhatrilo <ojhatrilochan5@gmail.com>
2024-04-05 10:14:13 +0530

    file

--- Python Daily Studies/FileIO.py
+++ Python Daily Studies/FileIO.py
@@ -1,32 +1,54 @@
 
 # f = open("demo.txt","w")
-
+#
 # f.write("Hello i have deleted all the above content")
-# f.close
+
 
 # f = open("demo.txt","r")
 # print(f.read())
 
 # f = open("demo.txt", "a+")
-
-# f.close() # read
+# f.write("\nhello iam elakiya")
+# f.close()
+#
+# read
 
 # f = open("demo.txt", "r")
 # print(f.read())
 
 
-input=open('demo.txt', 'r')
-output=open('demo2.txt', 'w+')
-text=input.read()
-z=text.split()
-y=[]
+# input=open('demo.txt', 'r')
+# output=open('demo2.txt', 'w+')
+# text=input.read()
+# z=text.split()
+# y=[]
+#
+# for i in z:
+#     if i not in y:
+#         y.append(i)
+#
+# x=" ".join(y)
+# output.write(x)
+# input.close()
+# output.close()
+#
 
-for i in z:
-    if i not in y:
-        y.append(i)
+# create file
 
-x=" ".join(y)  
-output.write(x)
-input.close()
+file = open("myfile.txt",'r')
+output = open("myfile2.txt",'w')
+#
+# file.write("hi my student is always taking leave for exam \nso i will be teaching new concepts \nwithout going backward")
+#
+# file.close()
+input = file.read()
+
+# print(input.replace(" ",""))
+
+z = input.replace(" her","her")
+output.write(z)
+file.close()
 output.close()
-        
+
+
+
