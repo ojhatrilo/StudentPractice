f25a8db7be41bb7dac60769c8d4b908ba309605b
ojhatrilo <ojhatrilochan5@gmail.com>
2024-04-18 19:52:17 +0530

    update

--- Python Daily Studies/demo.py
+++ Python Daily Studies/demo.py
@@ -27,39 +27,11 @@
 #     print(quotient[i],end=' ')
 
 
-a = "hello every one"
-x = a[12:15]
-# print(x)
-y = x[::-1]
-print(y)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# a = "hello every one"
+# x = a[12:15]
+# # print(x)
+# y = x[::-1]
+# print(y)
 
 # num = [2,4,8]
 #
@@ -74,6 +46,77 @@ print(y)
 #
 # for i in range(0,len(num),2):
 #     print(quotient[i],end=' ')
+
+
+#
+# class calculator:
+#     def __init__(self,num1,num2):
+#         self.num1 = num1
+#         self.num2 = num2
+#
+#     def add(self):
+#         return self.num1 + self.num2
+#
+#     def sub(self):
+#         return self.num2 - self.num1
+#
+#     def mul(self):
+#         return self.num1 * self.num2
+#
+#     def div(self):
+#         return self.num2 / self.num1
+#
+#
+# while(True):
+#     options = input("enter the options:")
+#     a = int(input("enter the number:"))
+#     b = int(input("enter the number:"))
+#     obj = calculator(a,b)
+#
+#     if options == "1":
+#         print(obj.add())
+#     elif options == "2":
+#         print(obj.sub())
+#     elif options == "3":
+#         print(obj.mul())
+#     elif options == "4":
+#         print(obj.div())
+#     elif options == "q":
+#         break
+#
+
+
+
+list1 = [25,44,55,72]
+
+temp  =  list1[0]
+list1[0] = list1[-1]
+list1[-1] = temp
+
+print(list1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
