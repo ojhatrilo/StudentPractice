caa87a62a0380e9ad233b6b64d6a4c1db28acec5
ojhatrilo <ojhatrilochan5@gmail.com>
2024-05-03 19:50:58 +0530

    db update

--- Python Daily Studies/db.py
+++ Python Daily Studies/db.py
@@ -11,9 +11,29 @@ mydb = mysql.connector.connect(
 
 data = mydb.cursor()
 
-data.execute("show databases")
+# data.execute("show databases")
+#
+# for i in data:
+#     print(i)
 
-for i in data:
-    print(i)
+# point = mydb.cursor()
+# point.execute("show databases")
 
+# sql = "create database if not exists soni"
+# sql = "create table if not exists data (name varchar(255), age INT, DOB Date )"
+
+# sql = "insert into data (name,age,DOB) VALUES (%s,%s,%s)"
+# val = [("somiya",21,'2002-04-05'),
+#        ("nithiya",24,'2000-01-30')
+#        ]
+
+# sql = "select * from data"
+# point.execute(sql)
+# for i in point:
+#     print(i)
+
+
+
+# for i in point:
+#     print(i)
 
