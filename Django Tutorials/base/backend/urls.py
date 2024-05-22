e574b1ef9f44853fe7639a5c51aad8023753093b
Ojhatrilo <ojhatrilochan5@gmail.com>
2024-05-22 13:20:57 +0530

    upload

--- Django Tutorials/base/backend/urls.py
+++ Django Tutorials/base/backend/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.homepage, name="index"),
     path('about/', views.about, name="hello"),
+    path('contact/<id>', views.data, name="contact"),
 ]
