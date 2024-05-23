5c9f6f46f56a47634ea20d0310e9321b48bf904b
ojhatrilo <ojhatrilochan5@gmail.com>
2024-05-23 19:54:55 +0530

    authentication

--- Student Demo/backend/base/views.py
+++ Student Demo/backend/base/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse,JsonResponse
 from .models import Musician,Album
+from django.contrib.auth import authenticate, login, logout
 
 
 # Create your views here.
@@ -11,14 +12,15 @@ def hello(request):
 
 def hi(request):
     # query = Album.objects.get(id = id)
-    if request.method =="POST":
-        fname = request.POST.get('fname')
-        lname = request.POST.get('lname')
-        instrument = request.POST.get('instrument')
+    if request.user.is_authenticated:
+        if request.method =="POST":
+            fname = request.POST.get('fname')
+            lname = request.POST.get('lname')
+            instrument = request.POST.get('instrument')
 
-        value = Musician(first_name = fname,last_name = lname,instrument = instrument)
-        value.save()
-        return redirect('home')
+            value = Musician(first_name = fname,last_name = lname,instrument = instrument)
+            value.save()
+            return redirect('home')
     return render(request, 'famous.html')
 
 def viewdata(request,id):
