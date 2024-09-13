4dc894663d6fe81ccf03e414840ad6fbb143bcdf
Ojhatrilo <ojhatrilochan5@gmail.com>
2024-09-13 19:48:36 +0530

    update

--- Python Daily Studies/Task_Tracker.py
+++ Python Daily Studies/Task_Tracker.py
@@ -6,49 +6,68 @@
 
 
 import json
-# from enum import Enum
+from enum import Enum
 
-# class Status(Enum):
-#     COMPLETED = 1
-#     PENDING = 2
+class Status(Enum):
+    COMPLETED = 1
+    PENDING = 2
   
 
-# # Creating Task
+# Creating Task
 # def Create_Task():
-#     id = int(input("Enter your id:"))
-#     Task = input("Enter your Task:")
-#     print('''To insert task update
-#           1. COMPLETED
-#           2. PENDING''')
-#     Task_Status = int(input("Enter task update:"))
-#     if Task_Status == 1:
-#         Task_Status = Status(1).name
-#     elif Task_Status == 2:
-#         Task_Status = Status(2).name
+    # id = int(input("Enter your id:"))
+    # Task = input("Enter your Task:")
+    # print('''To insert task update
+    #       1. COMPLETED
+    #       2. PENDING''')
+    # Task_Status = int(input("Enter task update:"))
+    # if Task_Status == 1:
+    #     Task_Status = Status(1).name
+    # elif Task_Status == 2:
+    #     Task_Status = Status(2).name
 
-#     exact_data = open("data.json",'+a')
-#     data = {
-#         "id": id,
-#         "Task": Task,
-#         "Task_Status": Task_Status
-#         }
-#     info = json.dumps(data)
-#     exact_data.write(info,",")
+    # exact_data = open("data.json",'+a')
+    # file = json.load(exact_data)
+    # data = {
+    #     "id": id,
+    #     "Task": Task,
+    #     "Task_Status": Task_Status
+    #     }
+    # file["Task_Schedule"].append(data)
+    # json.dump(file,indent=4)
+    # exact_data.close()
+def write_json(new_data, filename='data.json'):
+    with open(filename,'r+') as file:
+          # First we load existing data into a dict.
+        file_data = json.load(file)
+        # Join new_data with file_data inside emp_details
+        file_data["emp_details"].append(new_data)
+        # Sets file's current position at offset.
+        file.seek(0)
+        # convert back to json.
+        json.dump(file_data, file, indent = 4)
+
+y = {"emp_name":"Nikhil",
+     "email": "nikhil@geeksforgeeks.org",
+     "job_profile": "Full Time"
+    }
+    
+write_json(y) 
 
 
-# Create_Task()
+
 
 
 
 # Read Task
-def Read_Task():
-    id = int(input("Enter the number:"))-1
-    data = open("data.json", "r")
-    data1 = json.load(data)
-    print(data1[id])
+# def Read_Task():
+#     id = int(input("Enter the number:"))-1
+#     data = open("data.json", "r")
+#     data1 = json.load(data)
+#     print(data1[id])
 
 
-Read_Task()
+# Read_Task()
 
 
 
