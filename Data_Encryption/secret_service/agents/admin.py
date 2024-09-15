d8490ddcb48316b9e2f860e519c241d97a8ea4ef
Ojhatrilo <ojhatrilochan5@gmail.com>
2024-09-15 15:46:02 +0530

    Encryption and Decryption

---
+++ Data_Encryption/secret_service/agents/admin.py
@@ -0,0 +1,10 @@
+
+
+# Register your models here.
+from django.contrib import admin
+from .models import Agent
+
+class AgentAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "code_name", "location", "secret_mission")
+
+admin.site.register(Agent, AgentAdmin)
